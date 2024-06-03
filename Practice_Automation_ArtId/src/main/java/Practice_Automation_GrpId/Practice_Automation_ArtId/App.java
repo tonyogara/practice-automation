3e463aa0e141672d241b2b2b664d67c3cb7ef912
Tony O Gara <tony.ogara@gmail.com>
2024-06-03 11:18:33 +0100

    committing the initial artifact to allow it to be pushed to the remote repository on Github

---
+++ Practice_Automation_ArtId/src/main/java/Practice_Automation_GrpId/Practice_Automation_ArtId/App.java
@@ -0,0 +1,13 @@
+package Practice_Automation_GrpId.Practice_Automation_ArtId;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
