cca5e4daa347498b197ff29cdddcb902448ce7d3
Tony O Gara <tony.ogara@gmail.com>
2024-06-04 11:59:39 +0100

    updated pom.xml and added config.properties and TestBase.java

---
+++ Practice_Automation_ArtId/src/main/java/com/practice_automation/qa/testbase/TestBase.java
@@ -0,0 +1,56 @@
+package com.practice_automation.qa.testbase;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestBase {
+	
+	public static String browserName;
+	public static WebDriver webDriver;
+	public static Properties prop = new Properties();
+	
+	
+	public TestBase(Properties prop)
+	{
+		try
+		{
+			FileInputStream ip = new FileInputStream(
+				"/Users/tony/Documents/Learning/Practice_Automation/Practice_Automation_ArtId/src/main/java/com/practice_automation/qa/config");
+			prop.load(ip);
+		} catch (FileNotFoundException e)
+			{
+				e.printStackTrace();
+			} catch (IOException e)
+			{
+				e.printStackTrace();			
+			}
+	}
+	
+	
+	
+	public void initialisation()
+	{
+		if (browserName.equalsIgnoreCase("Chrome"))
+		{
+				System.setProperty("webDriver.chrome.driver", "/Users/tony/Documents/Learning/FreeCrm/FreeCrmArtId/src/main/java/com/crm/qa/chromedriver/chromedriver");
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--remote-allow-origins=*");
+				webDriver = new ChromeDriver(options);
+		}
+		
+		
+		
+		webDriver.manage().window().maximize();
+		webDriver.get(prop.getProperty("url"));
+		
+	}
+	
+
+
+}
