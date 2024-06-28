7c5512c0d6461e876f0a826914aeb8174fc60c80
Tony O Gara <tony.ogara@gmail.com>
2024-06-28 14:10:14 +0100

    updated the i_select_the_submit_button() method to click the submit button. Also implemented the i_see_the_correct_message_being_displayed() method to check that the correct message is displayed after the submit button is selected

--- Practice_Automation_ArtId/src/test/java/com/practice_automation/qa/stepdefinitions/PopulateFormFieldPage.java
+++ Practice_Automation_ArtId/src/test/java/com/practice_automation/qa/stepdefinitions/PopulateFormFieldPage.java
@@ -1,9 +1,12 @@
 package com.practice_automation.qa.stepdefinitions;
 
+import static org.testng.Assert.assertEquals;
+
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -32,36 +35,38 @@ public class PopulateFormFieldPage extends TestBase{
 	    homePage.selectFormFieldPage();
 	}
 	
-	//By id
+	
 	@When("I enter the name")
 	public void i_enter_the_name() {
 		webDriver.findElement(By.id("name")).sendKeys("Tony O'Gara");
 	}
 
-	//By id
+	
 	@When("I select the drinks")
 	public void i_select_the_drinks() {
 		webDriver.findElement(By.id("drink2")).click();
 	}
 
 	
-	//By tagname
 	@When("I select a colour")
-	public void i_select_a_colour() {
-		webDriver.findElement(By.id("color1")).click();
+	public void i_select_a_colour() {		
+		WebElement element = webDriver.findElement(By.id("color3"));
+		Actions actions = new Actions(webDriver);
+		actions.moveToElement(element).click().perform();
 	}
 
+	
 	@When("I select yes to having a siblings")
 	public void i_select_yes_to_having_a_siblings() {
 		webDriver.findElement(By.id("siblings")).sendKeys("Maybe");
-
 	}
 
+	
 	@When("I enter an email address")
 	public void i_enter_an_email_address() {
 		webDriver.findElement(By.cssSelector("#email")).sendKeys("tony.ogara@gmail.com");
-		
 	}
+	
 	
 	//By tagname
 	@When("I enter a message")
@@ -72,25 +77,21 @@ public class PopulateFormFieldPage extends TestBase{
 
 	@When("I select the submit button")
 	public void i_select_the_submit_button() {
-		//webDriver.findElement(By.id("submit-btn")).click();
-		WebElement subBtn = webDriver.findElement(By.cssSelector("#submit-btn"));
-		System.out.println("Before");
-		//WebDriverWait wait = new WebDriverWait(webDriver.d.Duration.ofSeconds(10));
-		
-		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
-		wait.until(ExpectedConditions.visibilityOf(webDriver.findElement(By.cssSelector("#submit-btn"))));
-		subBtn.click();
-		
-		//new WebDriverWait(webDriver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/b[1]"))); 
-		
-		//subBtn.click();
-		//webDriver.findElement(By.xpath("//button[@id='submit-btn']")).click();
-		System.out.println("After");
+		WebElement element = webDriver.findElement(By.cssSelector("#submit-btn"));
+		Actions actions = new Actions(webDriver);
+		actions.moveToElement(element).click().perform();
 	}
+	
 
 	@Then("I see the correct message being displayed")
 	public void i_see_the_correct_message_being_displayed() {
-		System.out.println("Nine");
+		String alertStr = webDriver.switchTo().alert().getText();
+		assertEquals("Message received!", alertStr);
+		webDriver.switchTo().alert().dismiss();
+		
+		String winStr = webDriver.getWindowHandle();
+		webDriver.switchTo().window(winStr);
+		webDriver.close();
 	}
 
 
